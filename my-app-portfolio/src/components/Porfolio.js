4d1142b6f8d3776c7cbdfd136b3f0b82b6536983
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-17 01:39:04 -0500

    portfolio images added; needs lots of styling; sort of got the nav bar colors a bit better

--- my-app-portfolio/src/components/Porfolio.js
+++ my-app-portfolio/src/components/Porfolio.js
@@ -1,30 +1,51 @@
 import React from 'react';
 import { FaGithub } from "react-icons/fa";
+let techBlogPhoto = "/assets/TechBlog.png";
+let abmrPhoto = "/assets/ABMR.png";
+let astrnmyPhoto = "/assets/astrnmy.png";
+let employeeTrackerPhoto = "/assets/EmployeeTracker.png";
+let noteTakerExpressPhoto = "/assets/NoteTakerExpress.png";
+let socialNetworkAPIPhoto = "/assets/SocialNetworkAPI.png";
+let eCommerceBackendPhoto = "/assets/ECommerceBackend.png";
+let pwGeneratorPhoto = "/assets/PasswordGenerator.png";
 
 
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio Page</h1>
+      <h1>Portfolio</h1>
       <p>
         Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
         Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
       </p>
       <br></br>
 
+
       <a href="https://another-mvc-tech-blog.herokuapp.com/"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + techBlogPhoto} alt="Tech Blog Photo" height={400}></img>
         <div className="overlay"><span>Tech Blog</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/mvc-tech-blog"
         target="_blank" rel="noopener noreferrer">
         <FaGithub className="m-3" size={22} />
       </a>
-      <br></br>
-      <br></br>
+      <br></br><br></br>
 
+      <a href="https://another-bad-music-review-woo.herokuapp.com/"
+        target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + abmrPhoto} alt="ABMR Photo" height={350}></img>
+        <div className="overlay"><span>Another Bad Music Review</span></div>
+      </a>
+      <a href="https://github.com/blairrrrwho/another-bad-music-review"
+        target="_blank" rel="noopener noreferrer">
+        <FaGithub className="m-3" size={22} />
+      </a>
+
+      <br></br><br></br>
       <a href="https://blairrrrwho.github.io/astrnmy/"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + astrnmyPhoto} alt="astRnMy Photo" height={350}></img>
         <div className="overlay"><span>astRnMy</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/astrnmy"
@@ -33,18 +54,9 @@ export default function Portfolio() {
       </a>
       <br></br> <br></br>
 
-      <a href="https://another-bad-music-review-woo.herokuapp.com/"
+      <a href="https://github.com/blairrrrwho/employee-tracker-mysql"
         target="_blank" rel="noopener noreferrer">
-        <div className="overlay"><span>Another Bad Music Review</span></div>
-      </a>
-      <a href="https://github.com/blairrrrwho/another-bad-music-review"
-        target="_blank" rel="noopener noreferrer">
-        <FaGithub className="m-3" size={22} />
-      </a>
-      <br></br><br></br>
-
-      <a href="     https://github.com/blairrrrwho/employee-tracker-mysql"
-        target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + employeeTrackerPhoto} alt="Employee Tracker Photo" height={350}></img>
         <div className="overlay"><span>Employee Tracker</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/employee-tracker-mysql"
@@ -55,6 +67,7 @@ export default function Portfolio() {
 
       <a href="https://incredible-note-taker-express.herokuapp.com/"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + noteTakerExpressPhoto} alt="Note Taker Express Photo" height={350}></img>
         <div className="overlay"><span>Note Taker Express</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/note-taker-express"
@@ -65,6 +78,7 @@ export default function Portfolio() {
 
       <a href="https://github.com/blairrrrwho/neat-social-network-api"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + socialNetworkAPIPhoto} alt="Social Network API Photo" height={350}></img>
         <div className="overlay"><span>Social Network API - Backend with MongoDB</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/neat-social-network-api"
@@ -74,6 +88,7 @@ export default function Portfolio() {
 
       <a href="https://github.com/blairrrrwho/orm-e-commerce-backend"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + eCommerceBackendPhoto} alt="E-Commerce Backend Photo" height={350}></img>
         <div className="overlay"><span>E-Commerce Backend - ORM</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/orm-e-commerce-backend"
@@ -84,6 +99,7 @@ export default function Portfolio() {
 
       <a href="https://blairrrrwho.github.io/the-friendly-zebra-password-generator/"
         target="_blank" rel="noopener noreferrer">
+        <img src={process.env.PUBLIC_URL + pwGeneratorPhoto} alt="Password Generator Photo" height={350}></img>
         <div className="overlay"><span>Password Generator</span></div>
       </a>
       <a href="https://github.com/blairrrrwho/the-friendly-zebra-password-generator"
