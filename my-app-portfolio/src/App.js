905b54b061387ac21e64e2cc68d0380ba36b1ed8
Blair Millet <120157336+blairrrrwho@users.noreply.github.com>
2023-05-21 06:36:20 -0500

    everything is working; couldn't add .env for form id

--- my-app-portfolio/src/App.js
+++ my-app-portfolio/src/App.js
@@ -8,19 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <header className="App-header">
-        <p> 
-          <code>React Portfolio</code>
-        </p>
-        <br></br>
-        <a
-          className="App-link"
-          href="https://github.com/blairrrrwho"
-          target="_blank" rel="noopener noreferrer"
-        >
-          <code>blair millet</code>
-        </a>
-      </header> */}
       <PortfolioContainer />
       <Footer />
     </div>
--- my-app-portfolio/src/components/Contact.js
+++ my-app-portfolio/src/components/Contact.js
@@ -1,34 +1,50 @@
-// import React from 'react';
-
+import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import "../index.css"
 
 function Contact() {
-  const [state, handleSubmit] = useForm("mgebqnvp");
+
+  const [state, handleSubmit] = useForm("xayzzwke");
   if (state.succeeded) {
-    return <p>Thanks for contacting me!</p>;
+    return <p className='m-4'>Thanks for contacting me!</p>;
   }
+
   return (
     <section className="contact container p-4">
-      <h1>Contact Me</h1><br></br>
+      <h1>Contact Me</h1><br />
+
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name: </label><br></br>
-        <input placeholder='Name...' id="name" type="name" name="name" /> <br></br>
-        <ValidationError prefix="Name" field="name" errors={state.errors} /><br></br>
-        <label htmlFor="">Email Address: </label><br></br>
-        <input placeholder='Email...' id="email" type="email" name="email" /> <br></br>
-        <ValidationError prefix="Email" field="email" errors={state.errors} /> <br></br>
-        <label htmlFor='Message'>Message:</label><br></br>
-        <textarea placeholder='Message...' id="message" name="message" />
-        <ValidationError prefix="Message" field="message" errors={state.errors} />
-        <br />
+
+        <label htmlFor="Name">Name: </label><br />
+        <input placeholder='Name...' id="name" type="text" name="Name" required /> <br />
+        <ValidationError prefix="Name" field="name" errors={state.errors} /><br />
+
+        <label htmlFor="Phone">Phone Number: </label><br />
+        <input placeholder='Phone Number...' id="phone" type="phone" name="Phone"
+          pattern='^\d{10||11}$'
+          required /> <br />
+        <ValidationError prefix="Phone" field="phone" errors={state.errors} /> <br />
+
+        <label htmlFor="Email">Email Address: </label><br />
+        <input placeholder='Email...' id="email" type="email" name="Email" required /> <br />
+        <ValidationError prefix="Email" field="email" errors={state.errors} /> <br />
+
+        <label htmlFor='Subject'>Subject:</label><br />
+        <input placeholder='Subject...' id="subject" type="text" name="Subject" required /> <br />
+        <ValidationError prefix="Subject" field="subject" errors={state.errors} /><br />
+
+        <label htmlFor='Message'>Message:</label><br />
+        <textarea placeholder='Message...' id="message" type="text" name="Message" required />
+        <ValidationError prefix="Message" field="message" errors={state.errors} /><br />
+
         <button id='submitBtn' type="submit" disabled={state.submitting}>
-          Submit
+          Send Message
         </button>
+
       </form>
     </section>
 
   );
 }
-<div className="content"></div>
+
 export default Contact
